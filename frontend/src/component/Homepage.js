f4d4bdbade209b06033b556e69bd6ec16f268aae
Gulshan Verma <gulshanrock4477@gmail.com>
2024-12-31 20:34:49 +0530

    Your commit message

--- frontend/src/component/Homepage.js
+++ frontend/src/component/Homepage.js
@@ -1,22 +1,41 @@
-import React from 'react'
+// import React from 'react'
+// import { MDBCarousel, MDBCarouselItem } from 'mdb-react-ui-kit';
+
+// const Homepage = () => {
+//   return (
+//     <div>
+//       <MDBCarousel showControls interval={3000}>
+//         <MDBCarouselItem itemId={1} interval={1000} className='sliderimg'>
+//           <img src='/slider1.jpg' className='d-block w-100' alt='...' />
+//         </MDBCarouselItem>
+//         <MDBCarouselItem itemId={2} className='sliderimg'>
+//           <img src='/slider4.jpg' className='d-block w-100' alt='...' />
+//         </MDBCarouselItem>
+//         <MDBCarouselItem itemId={3} className='sliderimg'>
+//           <img src='slider3.jpg' className='d-block w-100' alt='...' />
+//         </MDBCarouselItem>
+//       </MDBCarousel>
+//     </div>
+//   )
+// }
+
+// export default Homepage
+
+import React from 'react';
 import { MDBCarousel, MDBCarouselItem } from 'mdb-react-ui-kit';
 
-const Homepage = () => {
+export default function Homepage() {
   return (
-    <div>
-      <MDBCarousel showControls interval={3000}>
-        <MDBCarouselItem itemId={1} interval={1000} className='sliderimg'>
-          <img src='/slider1.jpg' className='d-block w-100' alt='...' />
-        </MDBCarouselItem>
-        <MDBCarouselItem itemId={2} className='sliderimg'>
-          <img src='/slider4.jpg' className='d-block w-100' alt='...' />
-        </MDBCarouselItem>
-        <MDBCarouselItem itemId={3} className='sliderimg'>
-          <img src='slider3.jpg' className='d-block w-100' alt='...' />
-        </MDBCarouselItem>
-      </MDBCarousel>
-    </div>
-  )
+    <MDBCarousel showControls fade>
+      <MDBCarouselItem itemId={1} className='slider_img'>
+        <img src='/slide1.jpg' className='d-block w-100' alt='...' />
+      </MDBCarouselItem>
+      <MDBCarouselItem itemId={2} className='slider_img'>
+        <img src='/slide2.jpg' className='d-block w-100' alt='...' />
+      </MDBCarouselItem>
+      <MDBCarouselItem itemId={3} className='slider_img'>
+        <img src='slide3.jpg' className='d-block w-100' alt='...' />
+      </MDBCarouselItem>
+    </MDBCarousel>
+  );
 }
-
-export default Homepage
--- frontend/src/component/ProductPage.js
+++ frontend/src/component/ProductPage.js
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const ProductPage = () => {
+
+  const [message, setMessage] = useState("")
+  const [product, setProduct] = useState([])
+
   return (
-    <div>
-        <h1>Products Page</h1>
-        <p>Here are our amazing products!</p>
+    <div className='container'>
+
+      <h1>Products Page</h1>
+      <p>Here are our amazing products!</p>
+
+      <div className='row'>
+        <div className='col-md-4'>
+
+          <img src='' alt='...' />
+          <h4 className='m-0'>product</h4>
+          <p className='m-0'>this is product page</p>
+          <h6>$0 <span>$10</span></h6>
+
+        </div>
+      </div>
+
     </div>
   )
 }
